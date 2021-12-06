4d70e25f6b59a57883835a9e737990259de23272
Daniel Laury <DanSLaury@gmail.com>
2021-12-06 14:54:51 -0600

    fixed to include all cards

--- day_4/day_4a.py
+++ day_4/day_4a.py
@@ -1,12 +1,18 @@
+import numpy as np
 with open('day_4\input.txt') as f:
-    lines = []
+    numbers = []
+    cards = []
     row = []
-    lines.append(list(int(x) for x in f.readline().strip().split(',')))
+    numbers.append(list(int(x) for x in f.readline().strip().split(',')))
     f.readline()
     for entry in f:
         if entry == '\n':
-            lines.append(row)
+            cards.append(row)
             row = []
         else:
             row.append(list(filter(None, entry.strip('  ').strip().split(' '))))
-print(lines[1])
+            print(row)
+    cards.append(row)
+print("numbers: ", numbers)
+for i, x in enumerate(cards):
+    print(np.matrix(x), f" - card {i}")
