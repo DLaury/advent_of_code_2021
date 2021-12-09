83857b3d61efaf5a8bce30db933bd0d75106b72c
Daniel Laury <DanSLaury@gmail.com>
2021-12-08 19:07:23 -0600

    Day 5a Solution

--- day_5/day_5a.py
+++ day_5/day_5a.py
@@ -1,8 +1,43 @@
 import re
+import numpy as np
 
 with open('day_5\input.txt') as f:
-    lines = [list(map(int, re.split(' -> |,', line.strip()))) for line in f]
+    coords = [list(map(int, re.split(' -> |,', line.strip()))) for line in f]
 
-for i, points in enumerate(lines):
-    if (points[0] != points[2]) & (points[1] != points[3]):
-        lines.pop(i)
+coords = [x for x in coords if x[0] == x[2] or x[1] == x[3]]
+
+x_base = 0
+y_base = 0
+lines = []
+for i, points in enumerate(coords):
+    x1, y1, x2, y2 = points
+    if x1 == x2:
+        if y1 == y2:
+            lines.append([x1, y1])
+        elif y2 < y1:
+            for y in range(y2, y1+1):
+                lines.append([x1, y])
+        else:
+            for y in range(y1, y2+1):
+                lines.append([x1, y])
+    elif y1 == y2:
+        if x2 < x1:
+            for x in range(x2, x1+1):
+                lines.append([x, y1])
+        else:
+            for x in range(x1, x2+1):
+                lines.append([x, y1])
+    max_x = max(x1, x2)
+    max_y = max(y1, y2)
+    if max_x > x_base:
+        x_base = max_x
+    if max_y > y_base:
+        y_base = max_y
+
+grid = np.zeros((x_base+1, y_base+1), dtype=int)
+
+for point in lines:
+    grid[point[0]][point[1]] += 1
+
+more_than_1 = grid > 1
+print(more_than_1.sum())
