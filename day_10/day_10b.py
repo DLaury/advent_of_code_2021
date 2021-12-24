7252fc990e1f4c1df826d4674579b46003628c5c
Daniel Laury <DanSLaury@gmail.com>
2021-12-24 15:44:40 -0600

    Moved Day10b solution

---
+++ day_10/day_10b.py
@@ -0,0 +1,43 @@
+import re
+
+with open('day_10\input.txt') as f:
+    syntax = [list(x.strip()) for x in f]
+
+close = [')',']','}','>']
+close_re = r'[\]}\)>]'
+char_dict = {')': '(', ']': '[', '}': '{', '>': '<'}
+end_dict = {'(': ')', '[': ']', '{': '}', '<': '>'}
+val_dict = {')': 3, ']': 57, '}': 1197, '>': 25137}
+score_dict = {')': 1, ']': 2, '}': 3, '>': 4}
+total = []
+scores = []
+
+def check_for_closed(line):
+    pair_removed = False
+    for i, char in enumerate(line):
+        if char in close:
+            if line[i-1] == char_dict[char]:
+                line.pop(i)
+                line.pop(i-1)
+                # print(''.join(line))
+                pair_removed = True       
+    if pair_removed == True:
+        check_for_closed(line)
+    if any(i in line for i in close):
+        return re.search(close_re, ''.join(line)).group()
+    else:
+        return [*map(end_dict.get, line)][::-1]
+
+for syn in syntax:
+    temp = check_for_closed(syn)
+    val = 0
+    if type(temp) ==  list:
+        for x in temp:
+            val *= 5
+            val += score_dict[x]
+        scores.append(val)
+    else:
+        total.append(val_dict[temp])
+
+print(sum(total))
+print(sorted(scores)[len(scores) // 2])
